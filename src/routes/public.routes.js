36a07d8688be13905f6464e9bba8ae6a0b9503a0
Deriam Suarez <deriam.suarez@mixart.do>
2024-02-14 15:15:16 -0400

    all

--- src/routes/public.routes.js
+++ src/routes/public.routes.js
@@ -9,11 +9,11 @@ import {
 } from "@/pages";
 
 const routes = [
-  { path: "/login", view: Login, alwaysVisible: false },
-  { path: "/register", view: Register, alwaysVisible: false },
-  { path: "/reset-password/:token", view: ResetPassword, alwaysVisible: false },
-  { path: "/forgot-password", view: ForgotPassword, alwaysVisible: false },
-  { path: "/confirm-email", view: ConfirmEmail, alwaysVisible: false },
+  // { path: "/login", view: Login, alwaysVisible: false },
+  // { path: "/register", view: Register, alwaysVisible: false },
+  // { path: "/reset-password/:token", view: ResetPassword, alwaysVisible: false },
+  // { path: "/forgot-password", view: ForgotPassword, alwaysVisible: false },
+  // { path: "/confirm-email", view: ConfirmEmail, alwaysVisible: false },
   { path: "/", view: Home, alwaysVisible: true },
   { path: "*", view: NotFound, alwaysVisible: true },
 ];
